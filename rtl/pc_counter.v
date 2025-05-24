d20db8239561eb2ae99d3c5830eddd6afc51ab51
Kagan Dikmen <kagandikmen@outlook.com>
2025-05-25 00:27:01 +0200

    adapt pc counter logic for bram program memory

--- rtl/pc_counter.v
+++ rtl/pc_counter.v
@@ -1,6 +1,6 @@
 // PC Counter of the CPU
 // Created:     2024-01-25
-// Modified:    2025-05-23
+// Modified:    2025-05-24
 // Author:      Kagan Dikmen
 
 module pc_counter
@@ -17,24 +17,28 @@ module pc_counter
 
     output [OPD_WIDTH-1:0] pc_out,
     output [OPD_WIDTH-1:0] pc_plus4,
-    output [PC_WIDTH-1:0] next_pc
+    output reg [PC_WIDTH-1:0] next_pc
     );
 
     reg [31:0] pc;
-    reg rst_buff;
     wire [PC_WIDTH-1:0] next_pc_buffer;
+    reg rst_buff;
+
+    always @(*)
+    begin
+        if (rst == 1'b1 || rst_buff == 1'b1)
+            next_pc = 32'b0;
+        else
+            next_pc = {'b0, next_pc_buffer};
+    end
 
     always @(posedge clk)
     begin
+        pc <= next_pc;
         rst_buff <= rst;
-        if (rst == 1'b1 || rst_buff == 1'b1)
-            pc <= 32'b0;
-        else
-            pc <= {'b0, next_pc_buffer};
     end
 
     assign next_pc_buffer = ((branch == 1'b1 && comp_result == 'b1) || jump == 1'b1) ? alu_result : pc + 4;
-    assign next_pc = next_pc_buffer;
     assign pc_out = pc;
     assign pc_plus4 = pc + 4;
 
