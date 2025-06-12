24988f777554dff97bd4774256edc352e3019ad4
Francesco Urbani <18237108+urbanij@users.noreply.github.com>
2025-06-12 09:33:25 +0200

    Using precompiled RISC-V toolchain

--- .github/setup-ci.sh
+++ .github/setup-ci.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 apt-get update
-apt-get install autoconf automake autotools-dev curl python3 python3-pip libmpc-dev libmpfr-dev libgmp-dev gawk build-essential bison flex texinfo gperf libtool patchutils bc zlib1g-dev libexpat-dev ninja-build git cmake libglib2.0-dev libslirp-dev
+apt-get install npm autoconf automake autotools-dev curl python3 python3-pip libmpc-dev libmpfr-dev libgmp-dev gawk build-essential bison flex texinfo gperf libtool patchutils bc zlib1g-dev libexpat-dev ninja-build git cmake libglib2.0-dev libslirp-dev
 apt update
 apt install iverilog
+npm install --global xpm@latest
